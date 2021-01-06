3e3e00a584bc2910198d2204a3f4b51c7a68c890
itzplayz0001 <66119667+itzplayz0001@users.noreply.github.com>
2021-01-06 20:52:24 +0530

    Create lana-rhoades.cs
    TOP-SECRET

---
+++ lana-rhoades.cs
@@ -0,0 +1,18 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		
+		/* Samuel Miller */
+		string samuelMiller = "Hello";	
+		string has = ", ";
+		string a = "Worl";
+		string small = "d";
+		string pp = "!";
+		Console.WriteLine(samMiller + has + a + small + pp);
+		
+		Console.ReadLine();
+	}
+}
